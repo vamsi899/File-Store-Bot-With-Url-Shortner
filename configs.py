3316160ec71d5ca80dcd08f884fd57a94217f40e
vamsi899 <krishnavamsibakka09@gmail.com>
2025-04-24 14:24:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,29 +1,28 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+  API_ID = int(os.environ.get("API_ID", "29055333"))
+  API_HASH = os.environ.get("API_HASH", "a6d154242eaef80a163bf5d0a7763882")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7795414140:AAHPYxU-Yc7AJ3KcR-zWZmqFOYd4C_mNItQ")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "https://t.me/Cartoonworldtelugu_india09_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6828129421"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Beybladefansz:vqvwq2EB8jyaZghK@cluster0.tayq1ta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "1002024495069")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1002556802708"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+This is a Permanent FileStore Bot Created by Vamsi.Others Can use me by joining my Updates Channel 
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](https://t.me/Cartoonworldtelugu_india09_bot)
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
