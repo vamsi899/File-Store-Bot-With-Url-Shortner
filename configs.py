3c92683f130aae1dd518c7cac26ec1443a53a661
vamsi899 <krishnavamsibakka09@gmail.com>
2025-04-26 11:50:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -16,7 +16,7 @@ class Config(object):
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot Created by Vamsi.Others Can use me by joining my Updates Channel 
 
